99fd69d5af28c4bbc8ec67d6187223cc6e8cdb44
LJJ <397160015@qq.com>
2018-10-26 11:58:57 +0800

    Initial commit

---
+++ src/main/java/top/ljjapp/suanfa/SuanfaApplication.java
@@ -0,0 +1,12 @@
+package top.ljjapp.suanfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuanfaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuanfaApplication.class, args);
+    }
+}
