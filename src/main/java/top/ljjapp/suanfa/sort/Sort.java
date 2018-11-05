8208d37ff8675328c633c086275e236ae44ebec9
LJJ <397160015@qq.com>
2018-11-05 15:53:59 +0800

    堆排序

--- src/main/java/top/ljjapp/suanfa/sort/Sort.java
+++ src/main/java/top/ljjapp/suanfa/sort/Sort.java
@@ -24,7 +24,12 @@ public class Sort {
 //            System.out.print(i + "  ");
 //        }
         //4,希尔排序
-        shellSort(array);
+//        shellSort(array);
+//        for(int i : array){
+//            System.out.print(i + "  ");
+//        }
+        //5,堆排序
+        heapSort(array);
         for(int i : array){
             System.out.print(i + "  ");
         }
@@ -114,5 +119,46 @@ public class Sort {
                 }
             }
         }
+    }
+
+    /**
+     * 堆排序
+     * 堆排序的基本思想是：将待排序序列构造成一个大顶堆，此时，整个序列的最大值就是堆顶的根节点。将其与末尾元素进行交换，此时末尾就为最大值。
+     * 然后将剩余n-1个元素重新构造成一个堆，这样会得到n个元素的次小值。如此反复执行，便能得到一个有序序列了
+     * 思路：a.将无需序列构建成一个堆，根据升序降序需求选择大顶堆或小顶堆;
+     * b.将堆顶元素与末尾元素交换，将最大元素"沉"到数组末端;
+     * c.重新调整结构，使其满足堆定义，然后继续交换堆顶元素与当前末尾元素，反复执行调整+交换步骤，直到整个序列有序。
+     * @param arr
+     */
+    public void heapSort(int [] arr){
+        //1.构建大顶堆
+        for(int i = arr.length / 2-1; i >= 0; i--){
+            //从第一个非叶子结点从下至上，从右至左调整结构
+            adjustHeap(arr, i, arr.length);
+        }
+        //2.调整堆结构+交换堆顶元素与末尾元素
+        for(int j = arr.length-1; j > 0; j--){
+            SortUtil.swap(arr, 0, j);//将堆顶元素与末尾元素进行交换
+            adjustHeap(arr, 0, j);//重新对堆进行调整
+        }
+    }
+
+    /**
+     * 调整大顶堆（仅是调整过程，建立在大顶堆已构建的基础上）
+     */
+    public void adjustHeap(int []arr,int i,int length){
+        int temp = arr[i];//先取出当前元素i
+        for(int k = i*2+1; k < length; k = k*2+1){//从i结点的左子结点开始，也就是2i+1处开始
+            if(k+1 < length && arr[k] < arr[k+1]){//如果左子结点小于右子结点，k指向右子结点
+                k++;
+            }
+            if(arr[k] > temp){//如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
+                arr[i] = arr[k];
+                i = k;
+            }else{
+                break;
+            }
+        }
+        arr[i] = temp;//将temp值放到最终的位置
     }
 }
